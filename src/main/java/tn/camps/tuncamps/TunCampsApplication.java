02d2510d7f91970504af09dee7f299ac22b6f675
lakhalEya <lakhal.eya@esprit.tn>
2023-05-22 22:49:34 +0100

    First commit

---
+++ src/main/java/tn/camps/tuncamps/TunCampsApplication.java
@@ -0,0 +1,13 @@
+package tn.camps.tuncamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunCampsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunCampsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/camps/tuncamps/TunCampsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.camps.tuncamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunCampsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
