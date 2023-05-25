8a9c4f9ce9ffd045728fa8aa7dc89b1b3fababec
lakhalEya <lakhal.eya@esprit.tn>
2023-05-25 19:38:13 +0100

    test test

--- backend/src/main/java/tn/camps/tuncamps/TunCampsApplication.java
+++ backend/src/main/java/tn/camps/tuncamps/TunCampsApplication.java
@@ -8,6 +8,7 @@ public class TunCampsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TunCampsApplication.class, args);
+        System.out.println("Hello Hello ");
     }
 
 }
