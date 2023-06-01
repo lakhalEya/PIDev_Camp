f62703ee5ccc2f27c03b11a7458fdaae32e9ea7d
maynef <maysa.nefzi@esprit.tn>
2023-06-01 23:05:48 +0100

    delete package

--- backend/src/main/java/tn/camps/tuncamps/Services/Classes/Booking/ReservationService.java
+++
@@ -1,4 +0,0 @@
-package main.java.tn.camps.tuncamps.Services.Classes.Booking;
-
-public class ReservationService {
-}
--- backend/src/main/java/tn/camps/tuncamps/Services/Interfaces/Booking/ReservationImpl.java
+++
@@ -1,4 +0,0 @@
-package main.java.tn.camps.tuncamps.Services.Interfaces.Booking;
-
-public interface ReservationImpl {
-}
