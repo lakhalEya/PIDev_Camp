7086aa683edad464fc57eb7d671c99413e9c00f5
lakhalEya <lakhal.eya@esprit.tn>
2023-06-05 21:41:31 +0100

    not found page

--- frontend/src/app/user/user-routing.module.ts
+++ frontend/src/app/user/user-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { NotFoundComponent } from './../shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserLayoutComponent},
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
