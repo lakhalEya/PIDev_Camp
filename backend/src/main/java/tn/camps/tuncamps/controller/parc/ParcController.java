e113d8cc1dd16b742d00679492456529f58ee837
lakhalEya <lakhal.eya@esprit.tn>
2023-07-02 04:58:37 +0100

    add parc services

--- backend/src/main/java/tn/camps/tuncamps/controller/parc/ParcController.java
+++ backend/src/main/java/tn/camps/tuncamps/controller/parc/ParcController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tn.camps.tuncamps.persistence.entity.commun.Location;
 import tn.camps.tuncamps.persistence.entity.parc.Parc;
+import tn.camps.tuncamps.persistence.entity.parc.ParcCategory;
 import tn.camps.tuncamps.service.parc.ParcService;
 
 import java.util.List;
@@ -41,10 +43,17 @@ public class ParcController {
     }
 
     @DeleteMapping("/{parcId}")
-    public ResponseEntity<Void> deleteParc(@PathVariable int parcId) {
-        parcService.deleteParc(parcId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<?> deleteParc(@PathVariable int parcId) {
+        try {
+            parcService.deleteParc(parcId);
+        return new ResponseEntity<>("Parc " + parcId + " Deleted successfully", HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("Parc " + parcId + " is not found ", HttpStatus.NOT_FOUND);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>("Failed to delete Parc: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 
     @GetMapping("/{parcId}")
     public ResponseEntity<Parc> getParcById(@PathVariable int parcId) {
@@ -56,5 +65,64 @@ public class ParcController {
     public ResponseEntity<List<Parc>> getAllParcs() {
         List<Parc> parcs = parcService.getAllParcs();
         return new ResponseEntity<>(parcs, HttpStatus.OK);
+    }
+
+    @PutMapping("/disable/{id}")
+    public ResponseEntity<?> disableParc(@PathVariable int id) {
+        try {
+            Parc parc = parcService.disableParc(id);
+            return new ResponseEntity<>(parc, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/enable/{id}")
+    public ResponseEntity<?> enableLocation(@PathVariable int id) {
+        try {
+            Parc parc = parcService.enableParc(id);
+            return new ResponseEntity<>(parc, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/country/{country}")
+    public ResponseEntity<?> getLocationsByCountry(@PathVariable String country) {
+        List<Parc> parcList = parcService.getParcByCountry(country);
+        return new ResponseEntity<>(parcList, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/sortBy/{sortBy}")
+    public ResponseEntity<?> getAllLocationsFiltredBy(@PathVariable String sortBy) {
+        List<Parc> locationList = parcService.getAllParcsSorted(sortBy);
+        return new ResponseEntity<>(locationList, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/filterBy/{filterBy}/{filterValue}")
+    public ResponseEntity<?> getAllLocationsFiltredBy(@PathVariable String filterBy, @PathVariable String filterValue) {
+        List<Parc> locationList = parcService.getAllParcsFiltered(filterBy, filterValue);
+        return new ResponseEntity<>(locationList, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/availabe/{status}")
+    public ResponseEntity<?> findLocationByDisponibility(@PathVariable Parc.ParcStatus status) {
+        List<Parc> locationList = parcService.getParcByDisponibility(status);
+        return new ResponseEntity<>(locationList, HttpStatus.OK);
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<?> findLocationByCategory(@PathVariable ParcCategory category) {
+        List<Parc> locationList = parcService.getParcByCategory(category);
+        return new ResponseEntity<>(locationList, HttpStatus.OK);
+    }
+
+    @GetMapping("/amenities/{amenities}")
+    public ResponseEntity<?> findLocationByAmenities(@PathVariable String amenities) {
+        List<Parc> locationList = parcService.getParcByAmenities(amenities);
+        return new ResponseEntity<>(locationList, HttpStatus.OK);
     }
 }
--- backend/src/main/java/tn/camps/tuncamps/service/parc/impl/ParcServiceImpl.java
+++ backend/src/main/java/tn/camps/tuncamps/service/parc/impl/ParcServiceImpl.java
@@ -1,15 +1,19 @@
 package tn.camps.tuncamps.service.parc.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tn.camps.tuncamps.persistence.entity.commun.Location;
 import tn.camps.tuncamps.persistence.entity.parc.Parc;
+import tn.camps.tuncamps.persistence.entity.parc.ParcCategory;
 import tn.camps.tuncamps.persistence.repository.commun.LocationRepository;
 import tn.camps.tuncamps.persistence.repository.parc.ParcRepository;
 import tn.camps.tuncamps.service.commun.LocationService;
 import tn.camps.tuncamps.service.parc.ParcService;
 
+import javax.persistence.criteria.Path;
 import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -76,8 +80,7 @@ public class ParcServiceImpl implements ParcService {
 
         parc.setCreationDate(existingParc.get().getCreationDate());
 
-        if (existingParc.get().getLocation() != null && (existingParc.get().getLocation().getLongitude() != parc.getLocation().getLongitude() ||
-                existingParc.get().getLocation().getLatitude() != parc.getLocation().getLatitude())) {
+        if (existingParc.get().getLocation() != null && (existingParc.get().getLocation().getLongitude() != parc.getLocation().getLongitude() || existingParc.get().getLocation().getLatitude() != parc.getLocation().getLatitude())) {
 
             Optional<Location> location = locationRepository.findByLatitudeAndLongitude(parc.getLocation().getLatitude(), parc.getLocation().getLongitude());
 
@@ -106,17 +109,89 @@ public class ParcServiceImpl implements ParcService {
 
     @Override
     public void deleteParc(int parcId) {
-        parcRepository.deleteById(parcId);
+        Optional<Location> parc = locationRepository.findById(parcId);
+        if (parc.isPresent()) {
+            try {
+                parcRepository.deleteById(parcId);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to delete parc with id: " + parcId, e);
+            }
+        } else {
+            throw new IllegalArgumentException("parc not found with id: " + parcId);
+        }
     }
+
 
     @Override
     public Parc getParcById(int parcId) {
-        return parcRepository.findById(parcId)
-                .orElseThrow(() -> new NoSuchElementException("Parc not found with ID: " + parcId));
+        return parcRepository.findById(parcId).orElseThrow(() -> new NoSuchElementException("Parc not found with ID: " + parcId));
     }
 
+    @Override
+    public List<Parc> getParcByCountry(String country) {
+        return parcRepository.findByLocationCityCountry(country);
+    }
+
+    @Override
+    public List<Parc> getAllParcsFiltered(String filterBy, String filterValue) {
+        Specification<Parc> spec = createParcSpecification(filterBy, filterValue);
+        return parcRepository.findAll(spec);
+    }
+
+    private Specification<Parc> createParcSpecification(String filterBy, String filterValue) {
+        return (root, query, criteriaBuilder) -> {
+            if (filterBy != null && filterValue != null) {
+                Path<String> filterField = root.get(filterBy);
+                return criteriaBuilder.like(filterField, "%" + filterValue + "%");
+            }
+            return null;
+        };
+    }
+
+    @Override
+    public List<Parc> getParcByCategory(ParcCategory category) {
+        return parcRepository.findByCategory(category);
+    }
+
+    @Override
+    public List<Parc> getParcByAmenities(String amenities) {
+        return parcRepository.findByAmenity(amenities);
+    }
+
+    @Override
+    public List<Parc> getParcByDisponibility(Parc.ParcStatus status) {
+        return parcRepository.findByStatus(status);
+    }
+    @Override
+    public List<Parc> getAllParcsSorted(String sortBy) {
+        Sort sort = Sort.by(sortBy);
+        return parcRepository.findAll(sort);
+    }
     @Override
     public List<Parc> getAllParcs() {
         return parcRepository.findAll();
     }
+
+
+    @Override
+    public Parc enableParc(int id) {
+        Optional<Parc> existingParc = parcRepository.findById(id);
+        if (!existingParc.isPresent())
+            throw new IllegalArgumentException("the location with the id + " + id + " doesn't exists.");
+        existingParc.get().setStatus(Parc.ParcStatus.DISABLED);
+        return parcRepository.save(existingParc.get());
+
+    }
+
+    @Override
+    public Parc disableParc(int id) {
+        Optional<Parc> existingParc = parcRepository.findById(id);
+        if (!existingParc.isPresent())
+            throw new IllegalArgumentException("the location with the id + " + id + " doesn't exists.");
+        existingParc.get().setStatus(Parc.ParcStatus.DISABLED);
+        return parcRepository.save(existingParc.get());
+
+    }
+
+
 }
